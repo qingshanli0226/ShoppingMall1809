f86e3696449b16625d724819238415f37891d2b5
hanqingyu <1922594647@qq.com>
2021-05-18 16:20:14 +0800

    5.18

--- app/src/main/java/com/shoppingmall/MainActivity.java
+++ app/src/main/java/com/shoppingmall/MainActivity.java
@@ -4,12 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.shoppingmall.framework.mvp.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initPresenter() {
+
+    }
+
+    @Override
+    protected void initData() {
+
     }
 }
--- net/src/main/java/com/shoppingmall/net/model/ShoppingMallApiService.java
+++ net/src/main/java/com/shoppingmall/net/model/ShoppingMallApiService.java
@@ -1,5 +1,13 @@
 package com.shoppingmall.net.model;
 
+import com.shoppingmall.net.bean.HomeBean;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
 public interface ShoppingMallApiService {
+    //http://49.233.0.68:8080/atguigu/json/HOME_URL.json
+    @GET("/atguigu/json/HOME_URL.json")
+    Observable<HomeBean> getHomeData();
 
 }
