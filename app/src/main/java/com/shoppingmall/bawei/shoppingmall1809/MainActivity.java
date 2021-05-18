2d5929dde9e41353816f7bf5f3bbbfa33faf31d4
liqingshan <liqingshan@sina.cn>
2021-05-18 18:26:54 +0800

    use androidX library

--- app/src/main/java/com/shoppingmall/bawei/shoppingmall1809/MainActivity.java
+++ app/src/main/java/com/shoppingmall/bawei/shoppingmall1809/MainActivity.java
@@ -1,7 +1,8 @@
 package com.shoppingmall.bawei.shoppingmall1809;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
