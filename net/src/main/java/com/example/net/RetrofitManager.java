ca2a8b15a2b7e223b22a6806098f7f5b3582cbda
zhaoyonghui <2513295133@qq.com>
2021-05-18 18:42:14 +0800

    net合

---
+++ net/src/main/java/com/example/net/RetrofitManager.java
@@ -0,0 +1,36 @@
+package com.example.net;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitManager {
+    private static Api api;
+
+    public static Api getApi() {
+        if (api==null){
+            api=createapi();
+        }
+        return api;
+    }
+
+    private static Api createapi() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .writeTimeout(2, TimeUnit.MINUTES)
+                .readTimeout(2, TimeUnit.MINUTES)
+                .connectTimeout(2, TimeUnit.MINUTES)
+                .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .client(okHttpClient)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit.create(Api.class);
+    }
+}
