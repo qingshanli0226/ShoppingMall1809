8bd2bed7a3fa7800bb8f50c23d3136e70c87ad0b
liyifan <3202327205@qq.com>
2021-05-18 14:10:43 +0800

    创建

---
+++ app/src/main/java/com/example/shoppingmall1809/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.shoppingmall1809;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
