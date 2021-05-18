f01c3068ba7b39f91b4de873bf7786da9e0f4b5c
zhaoyonghui <2513295133@qq.com>
2021-05-18 19:03:04 +0800

    注册1

--- net/src/main/java/com/example/net/Api.java
+++ net/src/main/java/com/example/net/Api.java
@@ -1,11 +1,18 @@
 package com.example.net;
 
 import com.example.net.bean.HomeBean;
+import com.example.net.bean.RegisterBean;
 
 import io.reactivex.Observable;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+
 public interface Api {
 @GET(Constants.BASE_URL)
     Observable<HomeBean> getHomebean();
-
+@POST("register")
+@FormUrlEncoded
+    Observable<RegisterBean> getRegister(@Field("name")String name,@Field("password")String password);
 }
