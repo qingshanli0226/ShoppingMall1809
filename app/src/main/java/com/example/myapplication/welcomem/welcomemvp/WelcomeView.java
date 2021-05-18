363cdf80a5b8b3a572f17f2ab5ae0c4e349dd58d
zhaoyonghui <2513295133@qq.com>
2021-05-18 20:25:04 +0800

    改错

--- app/src/main/java/com/example/myapplication/welcomem/welcomemvp/WelcomeView.java
+++ app/src/main/java/com/example/myapplication/welcomem/welcomemvp/WelcomeView.java
@@ -2,8 +2,8 @@ package com.example.myapplication.welcomem.welcomemvp;
 
 import com.example.net.bean.HomeBean;
 
-import mvp.view.BaseVuew;
+import mvp.view.BaseVIew;
 
-public interface WelcomeView extends BaseVuew {
+public interface WelcomeView extends BaseVIew {
     void onWelcome(HomeBean homeBean);
 }
