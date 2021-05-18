5e3f5511a30e4b6d291d82056cbb1e7e872de0fe
chengtianle <1157143564@qq.com>
2021-05-18 18:42:28 +0800

    接口第二次

--- net/src/main/java/com/shoppingmall/net/model/ShoppingMallApiService.java
+++ net/src/main/java/com/shoppingmall/net/model/ShoppingMallApiService.java
@@ -1,13 +1,22 @@
 package com.shoppingmall.net.model;
 
+import com.shoppingmall.net.bean.AccessoryBean;
+import com.shoppingmall.net.bean.BagBean;
 import com.shoppingmall.net.bean.BuyBean;
 import com.shoppingmall.net.bean.DialogBean;
+import com.shoppingmall.net.bean.DigitBean;
+import com.shoppingmall.net.bean.DressBean;
+import com.shoppingmall.net.bean.GameBean;
 import com.shoppingmall.net.bean.HomeBean;
+import com.shoppingmall.net.bean.Home_ProductsBean;
 import com.shoppingmall.net.bean.JacketBean;
 import com.shoppingmall.net.bean.LabelBean;
 import com.shoppingmall.net.bean.LoginBean;
+import com.shoppingmall.net.bean.OvercoatBean;
+import com.shoppingmall.net.bean.PantsBean;
 import com.shoppingmall.net.bean.RegisterBean;
 import com.shoppingmall.net.bean.SkirtBean;
+import com.shoppingmall.net.bean.StationeryBean;
 
 import io.reactivex.Observable;
 import retrofit2.http.Field;
@@ -35,7 +44,6 @@ public interface ShoppingMallApiService {
     //抢购图片
     @GET("/atguigu/json/NEW_POST_URL.json")
     Observable<BuyBean> getBuyData();
-
     //http://49.233.0.68:8080/atguigu/json/HOT_POST_URL.json
     //对话图片
     @GET("/atguigu/json/HOT_POST_URL.json")
@@ -46,5 +54,42 @@ public interface ShoppingMallApiService {
     //上衣
     @GET("/atguigu/json/JACKET_URL.json")
     Observable<JacketBean> getJacketData();
+    //下衣
+    @GET("/atguigu/json/PANTS_URL.json")
+    Observable<PantsBean> getPantsData();
+    //外套
+    @GET("/atguigu/json/OVERCOAT_URL.json")
+    Observable<OvercoatBean> getOvercoatData();
+    //配件
+    @GET("/atguigu/json/ACCESSORY_URL.json")
+    Observable<AccessoryBean> getAccessoryData();
+    //包包
+    @GET("/atguigu/json/BAG_URL.json")
+    Observable<BagBean> getBagData();
+
+    //装扮
+    @GET("/atguigu/json/DRESS_UP_URL.json")
+    Observable<DressBean> getDressData();
+
+    //居家宅品
+    @GET("/atguigu/json/HOME_PRODUCTS_URL.json")
+    Observable<Home_ProductsBean> getHomeProductsData();
+
+
+    //办公文具
+    @GET("/atguigu/json/STATIONERY_URL.json")
+    Observable<StationeryBean> getStationeryData();
+
+    //数码周边
+    @GET("/atguigu/json/DIGIT_URL.json")
+    Observable<DigitBean> getDigitData();
+
+    //游戏专区
+    @GET("/atguigu/json/GAME_URL.json")
+    Observable<GameBean> getGameData();
+
+
+
+
 
 }
