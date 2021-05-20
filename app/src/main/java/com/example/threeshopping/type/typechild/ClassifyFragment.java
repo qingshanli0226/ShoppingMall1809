dc04b070bf1d26928fd1f4c38efca2dd824dd9a7
zhaoziyu <2774410792@qq.com>
2021-05-20 09:09:17 +0800

    第二页面

---
+++ app/src/main/java/com/example/threeshopping/type/typechild/ClassifyFragment.java
@@ -0,0 +1,55 @@
+package com.example.threeshopping.type.typechild;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.framework.BaseFragment;
+import com.example.threeshopping.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ClassifyFragment extends BaseFragment {
+
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initPrensenter() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public void onClickCenter() {
+
+    }
+
+    @Override
+    public void onClickLeft() {
+
+    }
+
+    @Override
+    public void onClickRight() {
+
+    }
+}
