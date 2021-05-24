ac301b44bf1761443db7f08a8bb9208fbbfcc1d0
zhaoyuzan <2571772686@qq.com>
2021-05-24 10:31:03 +0800

    11

--- app/src/main/java/com/example/shoppingmallsix/Goodsactivity/GoodsActivity.java
+++ app/src/main/java/com/example/shoppingmallsix/Goodsactivity/GoodsActivity.java
@@ -1,6 +1,12 @@
 package com.example.shoppingmallsix.Goodsactivity;
 
 import android.content.Intent;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -24,6 +30,8 @@ public class GoodsActivity extends BaseActivity {
     private String name;
     private String figure;
     private String price;
+    private LinearLayout shopcar;
+    private android.widget.Button insertShopcar;
 
     @Override
     protected void initPresenter() {
@@ -40,16 +48,34 @@ public class GoodsActivity extends BaseActivity {
         list.add(goodAdapterBean);
         list.add(goodAdapterBean);
         goodsAdapter.notifyDataSetChanged();
-
     }
 
     @Override
     protected void initView() {
         toolbar = (ToolBar) findViewById(R.id.toolbar);
         moreRv = (RecyclerView) findViewById(R.id.moreRv);
+        shopcar = findViewById(R.id.shopcar);
+        insertShopcar = findViewById(R.id.insertShopcar);
         goodsAdapter = new GoodsAdapter(list);
         moreRv.setAdapter(goodsAdapter);
         moreRv.setLayoutManager(new LinearLayoutManager(this));
+        insertShopcar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupWindow popupWindow = new PopupWindow();
+                popupWindow.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+                popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+                popupWindow.setFocusable(true);
+                popupWindow.setOutsideTouchable(true);
+
+                View inflate = LayoutInflater.from(GoodsActivity.this).inflate(R.layout.item_particulars_pop, null);
+                popupWindow.setContentView(inflate);
+
+                popupWindow.showAsDropDown(toolbar,0,900,Gravity.BOTTOM);
+            }
+        });
+
+
     }
 
     @Override
@@ -57,5 +83,33 @@ public class GoodsActivity extends BaseActivity {
         return R.layout.activity_goods;
     }
 
+
+        @Override
+        public void onRightImgClick() {
+            super.onRightImgClick();
+            PopupWindow popupWindow = new PopupWindow();
+
+            popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+            popupWindow.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+            popupWindow.setOutsideTouchable(true);
+            popupWindow.setFocusable(true);
+
+
+
+            View inflate = LayoutInflater.from(this).inflate(R.layout.item_pop, null);
+            popupWindow.setContentView(inflate);
+
+
+            LinearLayout viewById = inflate.findViewById(R.id.toMain);
+            viewById.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    finish();
+                    popupWindow.dismiss();
+                }
+            });
+
+            popupWindow.showAsDropDown(toolbar);
+        }
 
 }
