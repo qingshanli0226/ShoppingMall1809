bece8f86585470d7047b14172b9c62fff82759fc
weipei <2747160127@qq.com>
2021-05-31 10:24:59 +0800

    zfb

--- pay/src/main/java/com/example/pay/user/findforpay/FindForPayActivity.java
+++ pay/src/main/java/com/example/pay/user/findforpay/FindForPayActivity.java
@@ -1,25 +1,86 @@
 package com.example.pay.user.findforpay;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alipay.sdk.app.PayTask;
 import com.blankj.utilcode.util.LogUtils;
 import com.example.framework.BaseActivity;
+import com.example.framework.db.MessageTable;
+import com.example.framework.manager.GreenManager;
 import com.example.framework.manager.ShoppingCarManager;
 import com.example.framework.view.BaseRVAdapter;
 import com.example.net.model.FindForBean;
+import com.example.net.model.OrderinfoBean;
+import com.example.net.model.RegisterBean;
+import com.example.pay.IPayView;
+import com.example.pay.PayActivity;
+import com.example.pay.PayPresenter;
 import com.example.pay.R;
 import com.example.pay.user.adapter.FindForAdapter;
 
 import java.util.List;
+import java.util.Map;
 
 @Route(path = "/pay/FindForPayActivity")
-public class FindForPayActivity extends BaseActivity{
+public class FindForPayActivity extends BaseActivity<PayPresenter> implements IPayView {
 
     private RecyclerView shopActPayRv;
+    private static final int SDK_PAY_FLAG = 1;
+    private Handler handler = new Handler() {
+        @SuppressLint("HandlerLeak")
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case SDK_PAY_FLAG:
+                    boolean isSucceed = false;
+                    Map<String, String> result = (Map<String, String>) msg.obj;
+                    String resultStatus = result.get("resultStatus");
+                    if (resultStatus.equals("9000")) {
+                        Toast.makeText(FindForPayActivity.this, "支付成功", Toast.LENGTH_SHORT).show();
+                        isSucceed = true;
+                        FindForBean.ResultBean resultBean = new FindForBean.ResultBean();
+                        resultBean.setTime(System.currentTimeMillis() + "");
+                        resultBean.setTradeNo(findForBean.getTradeNo());
+                        ShoppingCarManager.getInstance().addForSend(resultBean);
+                        finish();
+                    } else {
+                        if (resultStatus.equals("8000")) {
+                            Toast.makeText(FindForPayActivity.this, "支付结果确认中", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else if (resultStatus.equals("6001")) { //用户中途取消
+                            Toast.makeText(FindForPayActivity.this, "取消支付", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // 其他值就可以判断为支付失败
+                            Toast.makeText(FindForPayActivity.this, "支付失败", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                    OrderinfoBean.ResultBean orderinfoBeanResult = new OrderinfoBean.ResultBean(findForBean.getOrderInfo().toString(),findForBean.getTradeNo());
+                    httpPresenter.getConfirmServerPayResult(orderinfoBeanResult.getOutTradeNo(), orderinfoBeanResult.getOrderInfo(), isSucceed);
+                    String message = "支付失败";
+                    if (isSucceed) {
+                        message = "支付成功";
+                    }
+                    MessageTable messageTable = new MessageTable(null, message, System.currentTimeMillis() + "", true);
+                    GreenManager.getInstance().setMessage(messageTable);
+                    GreenManager.getInstance().addCount();
+                    break;
+            }
+
+        }
+    };
+    private  FindForBean.ResultBean findForBean ;
 
     @Override
     protected int getLayoutId() {
@@ -30,7 +91,7 @@ public class FindForPayActivity extends BaseActivity{
     protected void initData() {
         List<FindForBean.ResultBean> forPay = ShoppingCarManager.getInstance().getForPay();
         LogUtils.json(forPay);
-        if (forPay!=null){
+        if (forPay != null) {
             FindForAdapter findForPayAdapter = new FindForAdapter(forPay);
             shopActPayRv.setAdapter(findForPayAdapter);
 
@@ -41,10 +102,19 @@ public class FindForPayActivity extends BaseActivity{
                     AlertDialog.Builder builder = new AlertDialog.Builder(FindForPayActivity.this);
                     builder.setTitle(getResources().getString(R.string.hint));
                     builder.setMessage(getResources().getString(R.string.areYouSureYouWantToContinuePaying));
-                    builder.setPositiveButton(getResources().getString(R.string.confirm),(dialogInterface, i) -> {
+                    builder.setPositiveButton(getResources().getString(R.string.confirm), (dialogInterface, i) -> {
+                        findForBean = forPay.get(position);
+                        new Thread(() -> {
+                            PayTask payTask = new PayTask(FindForPayActivity.this);
+                            Map<String, String> result = payTask.payV2(findForBean.getOrderInfo().toString(), false);
+                            Message msg = new Message();
+                            msg.what = SDK_PAY_FLAG;
+                            msg.obj = result;
+                            handler.sendMessage(msg);
 
+                        }).start();
                     });
-                    builder.setNegativeButton(getResources().getString(R.string.cancel),(dialogInterface, i) -> {
+                    builder.setNegativeButton(getResources().getString(R.string.cancel), (dialogInterface, i) -> {
                     });
                     builder.show();
                 }
@@ -59,7 +129,7 @@ public class FindForPayActivity extends BaseActivity{
 
     @Override
     protected void initPresenter() {
-
+        httpPresenter = new PayPresenter(this);
     }
 
     @Override
@@ -69,5 +139,27 @@ public class FindForPayActivity extends BaseActivity{
         linearLayoutManager.setStackFromEnd(true);
         linearLayoutManager.setReverseLayout(true);
         shopActPayRv.setLayoutManager(linearLayoutManager);
+    }
+
+    @Override
+    public void getConfirmServerPayResult(RegisterBean registerBean) {
+        if (registerBean.getCode().equals("200")) {
+            Toast.makeText(this, "请求服务端成功", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    public void Error(String error) {
+        Toast.makeText(this, "" + error, Toast.LENGTH_SHORT).show();
     }
 }
