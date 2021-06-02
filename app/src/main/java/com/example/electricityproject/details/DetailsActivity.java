73040e5caece299ffd6e4ffde6974f374bdb2812
zhangruifei <3482464467@qq.com>
2021-06-02 15:21:17 +0800

    提交

--- app/src/main/java/com/example/electricityproject/details/DetailsActivity.java
+++ app/src/main/java/com/example/electricityproject/details/DetailsActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Path;
 import android.graphics.PathMeasure;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
