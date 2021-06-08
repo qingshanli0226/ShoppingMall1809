adeffacd69e5ca3a9f308ee306c94ee000930466
zhaoyonghui <2513295133@qq.com>
2021-06-08 15:58:27 +0800

    提交

--- app/src/main/java/com/example/myapplication/discover/DiscoverFragment.java
+++ app/src/main/java/com/example/myapplication/discover/DiscoverFragment.java
@@ -1,13 +1,12 @@
 package com.example.myapplication.discover;
 
 import android.content.Intent;
-import android.view.View;
 import android.widget.Button;
 
 import com.example.framework.BaseFragment;
-import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
 import com.example.myapplication.UmengActivity;
+import com.example.myapplication.live.LiveonActivity;
 import com.tencent.bugly.crashreport.CrashReport;
 
 
@@ -15,6 +14,7 @@ public class DiscoverFragment extends BaseFragment {
 
     private Button errorBtn;
     private Button btn;
+    private Button tui;
 
     @Override
     public int bandLayout() {
@@ -25,6 +25,7 @@ public class DiscoverFragment extends BaseFragment {
     public void initView() {
         errorBtn = (Button) findViewById(R.id.errorBtn);
         btn = (Button) findViewById(R.id.btn);
+        tui = (Button) findViewById(R.id.tui);
     }
 
     @Override
@@ -34,6 +35,9 @@ public class DiscoverFragment extends BaseFragment {
         btn.setOnClickListener(v -> {
             Intent intent = new Intent(getActivity(), UmengActivity.class);
             startActivity(intent);
+        });
+        tui.setOnClickListener(v ->{
+            startActivity(new Intent(getActivity(), LiveonActivity.class));
         });
     }
 
---
+++ app/src/main/java/com/example/myapplication/live/LiveonActivity.java
@@ -0,0 +1,51 @@
+package com.example.myapplication.live;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+import com.tencent.live2.V2TXLiveDef;
+import com.tencent.live2.V2TXLivePusher;
+import com.tencent.live2.impl.V2TXLivePusherImpl;
+import com.tencent.rtmp.ui.TXCloudVideoView;
+
+import static com.tencent.live2.V2TXLiveCode.V2TXLIVE_ERROR_INVALID_LICENSE;
+
+public class LiveonActivity extends AppCompatActivity {
+
+    private TXCloudVideoView pusherTxCloudView;
+    private V2TXLivePusher mLivePusher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liveon);
+        initView();
+         mLivePusher = new V2TXLivePusherImpl(this, V2TXLiveDef.V2TXLiveMode.TXLiveMode_RTMP); //指定对应的直播协议为 RTMP
+
+        //启动本地摄像头预览
+        mLivePusher.setRenderView(pusherTxCloudView);
+        mLivePusher.startCamera(true);
+        //启动推流
+        String rtmpURL = "rtmp://124108.livepush.myqcloud.com/live/livezhao?txSecret=010d938f966bac39a80efcc86bec228c&txTime=60C056FE"; //此处填写您的 rtmp 推流地址
+        int ret = mLivePusher.startPush(rtmpURL.trim());
+
+        if (ret == V2TXLIVE_ERROR_INVALID_LICENSE) {
+            Log.i("TAG", "startRTMPPush: license 校验失败");
+        }
+    }
+
+    private void initView() {
+        pusherTxCloudView = findViewById(R.id.pusher_tx_cloud_view);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //结束推流
+        mLivePusher.stopPush();
+    }
+
+}
